f3183b262cfeee3d77fc0c0d25cf82bdd28b98a5
IngCarlosRivero <212t0027@itsm.edu.mx>
2023-09-11 15:12:13 -0600

    Create ProductEntity.java
    Creacion de porduct Entity

---
+++ src/main/java/com/practica/productmicroservice/entity/ProductEntity.java
@@ -0,0 +1,8 @@
+package com.practica.productmicroservice.entity;
+
+public class ProductEntity {
+	private String id;
+	private String productName;
+	private String productDescription;
+	private Double unitPrice;
+	}
