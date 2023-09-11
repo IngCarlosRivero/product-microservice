71095ad7a627e32d3ab6435af0435652de65c846
IngCarlosRivero <212t0027@itsm.edu.mx>
2023-09-11 15:22:42 -0600

    Update ProductEntity.java
    Crear instancias

--- src/main/java/com/practica/productmicroservice/entity/ProductEntity.java
+++ src/main/java/com/practica/productmicroservice/entity/ProductEntity.java
@@ -1,5 +1,16 @@
 package com.practica.productmicroservice.entity;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(value = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class ProductEntity {
 	private String id;
 	private String productName;
